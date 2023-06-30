4e97260cb77ae05e36bbee55e7253e8569b86e6a
Abdul Ahad <abdulamgs@gmail.com>
2023-06-30 17:39:15 +0530

    refactor: made the form responsive

--- src/pages/TodoTask.vue
+++ src/pages/TodoTask.vue
@@ -1,48 +1,60 @@
 <template>
-    <v-sheet width="500" class="mx-auto">
-        <v-form ref="form" class="pa-10">
-            <v-text-field label="Title" v-model="name" :rules="nameRules"></v-text-field>
-            <v-text-field type="date" v-model="date" label="Expected completion date" :rules="dateRules"></v-text-field>
-            <v-textarea v-model="desc" label="Description" :rules="descRules"></v-textarea>
-            <v-btn type="submit" @click="validate" color="success">Create</v-btn>
-        </v-form>
+    <v-sheet class="mx-auto">
+        <v-container class="container">
+            <v-form ref="form" class="pa-10">
+                <v-row>
+                    <v-col cols="12" sm="12" lg="12" md="12" xsm="12">
+                        <v-text-field label="Title" v-model="name" :rules="nameRules">
+                        </v-text-field></v-col>
+                    <v-col cols="12" sm="12" lg="12" md="12" xsm="12">
+                        <v-text-field type="date" v-model="date" label="Expected completion date" :rules="dateRules">
+                        </v-text-field></v-col>
+                    <v-col cols="12" sm="12" lg="12" md="12" xsm="12">
+                        <v-textarea v-model="desc" label="Description" :rules="descRules">
+                        </v-textarea></v-col>
+                    <v-col cols="12" sm="12" lg="12" md="12" xsm="12">
+                        <v-btn type="submit" @click="validate" color="success">Create</v-btn>
+                    </v-col>
+                </v-row>
+            </v-form>
+        </v-container>
     </v-sheet>
 </template>
 
 <script>
 export default {
     name: 'TodoTask',
-    data(){
+    data() {
         return {
             valid: false,
             name: '',
             date: '',
             desc: '',
-            nameRules : [
+            nameRules: [
                 value => {
-                    if(value)
+                    if (value)
                         return true
 
                     return "name cant be blank"
                 }
             ],
-            descRules : [
+            descRules: [
                 value => {
-                    if(value)
+                    if (value)
                         return true
 
                     return "description can not be left blank"
                 },
                 value => {
-                    if(value.length < 50)
+                    if (value.length < 50)
                         return "please write valid description"
 
                     return true
                 }
             ],
-            dateRules : [
+            dateRules: [
                 value => {
-                    if(value)
+                    if (value)
                         return true
 
                     return "please select the date of completion for task"
@@ -51,19 +63,28 @@ export default {
         }
     },
     methods: {
-        async validate(){
+        async validate() {
             console.log("inside the validate method")
 
             const ans = await this.$refs.form.validate()
             console.log(ans.valid)
 
-            if(ans.valid){
+            if (ans.valid) {
                 this.$store.dispatch('addTodoMutation',
-                { name: this.name, 
-                completionDate: this.date, 
-                description: this.desc })
+                    {
+                        name: this.name,
+                        completionDate: this.date,
+                        description: this.desc
+                    })
             }
         }
     }
 }
 </script>
+
+<style>
+.container{
+    display: flex;
+    justify-content: center;
+}
+</style>
