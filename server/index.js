f744a6945c8c01a98ff7ae950cf40defaa2948ac
Abdul Ahad <abdulamgs@gmail.com>
2023-07-03 00:01:59 +0530

    initialized backend and configured the client for it

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+//middleware
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const tasks = require('./routes/api/tasks')
+
+const port = process.env.PORT || 5000
+
+app.use('/api/tasks', tasks)
+app.listen(port, () => {
+    console.log(`server started on port ${port}`)
+})
